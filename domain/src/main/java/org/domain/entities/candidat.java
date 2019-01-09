b8d642de01aa327746298000ef9d6888f74f8185
Henda88 <42939631+Henda88@users.noreply.github.com>
2019-01-09 20:59:30 +0100

    third commit

--- domain/src/main/java/org/domain/entities/candidat.java
+++ domain/src/main/java/org/domain/entities/candidat.java
@@ -1,7 +1,21 @@
 package org.domain.entities;
 
-public class candidat {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="candidat")
+public class candidat implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String id;
 	private String nom;
 	private String prenom;
