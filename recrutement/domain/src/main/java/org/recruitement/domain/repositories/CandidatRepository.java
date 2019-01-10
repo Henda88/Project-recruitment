8ac69613dc520fab0f4edd9c9bfde2e8dd243305
Henda88 <42939631+Henda88@users.noreply.github.com>
2019-01-10 14:17:24 +0100

    new interface

---
+++ recrutement/domain/src/main/java/org/recruitement/domain/repositories/CandidatRepository.java
@@ -0,0 +1,8 @@
+package org.recruitement.domain.repositories;
+
+import org.recruitement.domain.entities.candidat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CandidatRepository extends CrudRepository<candidat, Long>{
+
+}
